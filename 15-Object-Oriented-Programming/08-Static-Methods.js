ad13eaeccc0623797c25509e4598bed3550403cf
ShreyasMS <134408310+ShreyasMadappady@users.noreply.github.com>
2023-09-01 14:32:41 +0530

    Create 08-Static-Methods.js

---
+++ 15-Object-Oriented-Programming/08-Static-Methods.js
@@ -0,0 +1,37 @@
+//A static method in JavaScript is a method that belongs to the class itself, not to any of its instances. This means that static methods can be called without creating an instance of the class. Static methods are often used to define utility functions or create objects that contain default information.
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static method
+  static hey() {
+    console.log('Hey there 👋');
+    console.log(this);
+  }
+}
+
+PersonCl.hey(); // -> Hey there 👋
