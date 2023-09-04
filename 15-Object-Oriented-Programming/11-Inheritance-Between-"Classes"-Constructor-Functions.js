a1bfb67b94ffe4145990f7fd146cbbf4a1369bae
ShreyasMS <134408310+ShreyasMadappady@users.noreply.github.com>
2023-09-04 10:47:26 +0530

    Update 11-Inheritance-Between-"Classes"-Constructor-Functions.js

--- 15-Object-Oriented-Programming/11-Inheritance-Between-"Classes"-Constructor-Functions.js
+++ 15-Object-Oriented-Programming/11-Inheritance-Between-"Classes"-Constructor-Functions.js
@@ -13,22 +13,24 @@ const Student = function (firstName, birthYear, course) {
 };
 
 // Linking prototypes
+// this should be written first
 Student.prototype = Object.create(Person.prototype);
 
+// this should be written second
 Student.prototype.introduce = function () {
   console.log(`My name is ${this.firstName} and I study ${this.course}`);
 };
 
 const mike = new Student('Mike', 2020, 'Computer Science');
-mike.introduce();
-mike.calcAge();
+mike.introduce(); // -> My name is Mike and I study Computer Science
+mike.calcAge(); // -> 17
 
-console.log(mike.__proto__);
-console.log(mike.__proto__.__proto__);
+console.log(mike.__proto__); // -> Person {introduce: ƒ}
+console.log(mike.__proto__.__proto__); // -> {calcAge: ƒ, constructor: ƒ}
 
-console.log(mike instanceof Student);
-console.log(mike instanceof Person);
-console.log(mike instanceof Object);
+console.log(mike instanceof Student); // -> true
+console.log(mike instanceof Person); // -> true
+console.log(mike instanceof Object); // -> true
 
 Student.prototype.constructor = Student;
-console.dir(Student.prototype.constructor);
+console.dir(Student.prototype.constructor); // -> ƒ Student(firstName, birthYear, course)
