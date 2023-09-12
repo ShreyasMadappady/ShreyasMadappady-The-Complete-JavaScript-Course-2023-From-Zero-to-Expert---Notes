70754b11a4a89705cc8546c33bcb5f6af801309c
ShreyasMS <134408310+ShreyasMadappady@users.noreply.github.com>
2023-09-12 10:16:45 +0530

    Create 02-Welcome-to-Callback-Hell.js

---
+++ 17-Asynchronous-javascript:-Promises-AsyncORAwait,-and-AJAX/02-Welcome-to-Callback-Hell.js
@@ -0,0 +1,55 @@
+// -> So basically, callback hell is when we have a lot of nested callbacks in order to execute asynchronous tasks in sequence.
+
+// -> Now, the problem with callback hell is that it makes our code look very messy. But even more important, it makes our code harder to maintain, and very difficult to understand, and to reason about, and code that is hard to understand and difficult to reason about. Will have more bugs, and it's just worse code.
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryAndNeighbour('portugal');
+getCountryAndNeighbour('usa');
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+      setTimeout(() => {
+        console.log('4 second passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+//-> 1 second passed
+// 2 seconds passed
+// 3 second passed
+// 4 second passed
