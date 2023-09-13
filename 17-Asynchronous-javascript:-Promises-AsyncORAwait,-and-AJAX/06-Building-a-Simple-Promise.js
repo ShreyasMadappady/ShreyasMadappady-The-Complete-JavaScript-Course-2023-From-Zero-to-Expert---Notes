fc03b254e10f4cecffe364717a74c7a3ad974b72
ShreyasMS <134408310+ShreyasMadappady@users.noreply.github.com>
2023-09-13 10:27:06 +0530

    Create 06-Building-a-Simple-Promise.js

---
+++ 17-Asynchronous-javascript:-Promises-AsyncORAwait,-and-AJAX/06-Building-a-Simple-Promise.js
@@ -0,0 +1,59 @@
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lotter draw is happening 🔮');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 💰');
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 second passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 second passed'));
+
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+
+// -> Lotter draw is happening 🔮
+// demo.js:49 abc
+// demo.js:50 Error: Problem! at demo.js:50:16
+// demo.js:23 1 second passed
+// demo.js:12 Error: You lost your money 💩 at demo.js:7:14
+// demo.js:27 2 second passed
+// demo.js:31 3 second passed
+// demo.js:34 4 second passed
