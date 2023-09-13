5c4a540873ff0b806ff37a031d10671ddd19fac7
ShreyasMS <134408310+ShreyasMadappady@users.noreply.github.com>
2023-09-13 10:29:40 +0530

    Create 07-Promisifying-the-Geolocation-API.js

---
+++ 17-Asynchronous-javascript:-Promises-AsyncORAwait,-and-AJAX/07-Promisifying-the-Geolocation-API.js
@@ -0,0 +1,43 @@
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+// getPosition().then(pos => console.log(pos));
+
+const whereAmI = function () {
+  getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+
+      return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+
+      return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+      return res.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message} 💥`));
+};
+
+btn.addEventListener('click', whereAmI);
+
+
+// -> {distance: 'Throttled! See geocode.xyz/pricing', elevation: 'Throttled! See geocode.xyz/pricing', latt: 'Throttled! See geocode.xyz/pricing', city: 'Throttled! See geocode.xyz/pricing', prov: 'Throttled! See geocode.xyz/pricing', …}
+
+// demo.js:25 You are in Throttled! See geocode.xyz/pricing, undefined
