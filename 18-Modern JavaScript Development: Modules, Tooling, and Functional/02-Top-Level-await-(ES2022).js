f4b217365fdaecc06e3f6693ab1bb6a228952e16
ShreyasMS <134408310+ShreyasMadappady@users.noreply.github.com>
2023-09-15 14:10:50 +0530

    Update 02-Top-Level-await-(ES2022).js

--- 18-Modern JavaScript Development: Modules, Tooling, and Functional/02-Top-Level-await-(ES2022).js
+++ 18-Modern JavaScript Development: Modules, Tooling, and Functional/02-Top-Level-await-(ES2022).js
@@ -1,3 +1,6 @@
+// we can now use the await keyword outside of async functions, at least in modules So that's why this is here in the module section. So again, as I just said, because this is really important, we can now basically use the await keyword outside of an async function, which we call top-level await. Just remember that this only works in modules. If we were gonna try this in a normal script, like we have been using before this section, then top level await would still fail, so it would not work at all. But here in our HTML file, we can still see that we have our type set to module. So this is what is required in order to make top-level await actually work.
+
+
 // Top-Level Await (ES2022)
 
 // console.log('Start fetching');
